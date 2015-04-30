6b7344432fb8602f6648117989094af0e6c2b219
lincanbin <lincanbin@hotmail.com>
2015-04-30 14:41:26 +0800

    tools

---
+++ tools/getcolor.py
@@ -0,0 +1,14 @@
+#encoding=utf-8
+from xml.dom import minidom
+doc = minidom.parse("color.xml")
+root = doc.documentElement
+resources = root.getElementsByTagName("color")
+for color in resources:
+    #print "0x"+color.childNodes[0].nodeValue.replace("#","")
+    print "[",
+    print int("0x"+color.childNodes[0].nodeValue.replace("#","")[0:2],16),
+    print ",",
+    print int("0x"+color.childNodes[0].nodeValue.replace("#","")[2:4],16),
+    print ",",
+    print int("0x"+color.childNodes[0].nodeValue.replace("#","")[4:6],16),
+    print "],"
