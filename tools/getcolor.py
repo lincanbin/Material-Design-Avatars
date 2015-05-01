f4837f15ff910af51485407005e9051d8d898c84
lincanbin <lincanbin@hotmail.com>
2015-05-01 20:58:06 +0800

    Fixed compatibility issue in low version PHP(5.3 or lower)

--- tools/getcolor.py
+++ tools/getcolor.py
@@ -5,10 +5,10 @@ root = doc.documentElement
 resources = root.getElementsByTagName("color")
 for color in resources:
     #print "0x"+color.childNodes[0].nodeValue.replace("#","")
-    print "[",
+    print "array(",
     print int("0x"+color.childNodes[0].nodeValue.replace("#","")[0:2],16),
     print ",",
     print int("0x"+color.childNodes[0].nodeValue.replace("#","")[2:4],16),
     print ",",
     print int("0x"+color.childNodes[0].nodeValue.replace("#","")[4:6],16),
-    print "],"
+    print "),"
