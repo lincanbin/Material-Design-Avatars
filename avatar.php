9343f5d16e0615d221be0fc906c1753a88fe8d11
ZhaiXiaolei <zhaixiaolei@guazi.com>
2016-05-04 14:16:00 +0800

    增加Psr-4命名空间，方便在composer中直接依赖使用

--- avatar.php
+++ avatar.php
@@ -1,18 +1,18 @@
 <?php
-$TestData = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f',
-			 'g','h','j','k','l','z','x','c','v','b','n','m','0','1',
-			 '2','3','4','5','6','7','8','9','林','灿','斌','编','写',
-			 '于','二','零','一','五','年','四','月','三','十','日'];
-if(isset($_GET['char']) && $_GET['char'] != null ){
-	$Char = $_GET['char'];
-}else{
-	$Char = $TestData[mt_rand(0,count($TestData)-1)];
+$TestData = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f',
+             'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '0', '1',
+             '2', '3', '4', '5', '6', '7', '8', '9', '林', '灿', '斌', '编', '写',
+             '于', '二', '零', '一', '五', '年', '四', '月', '三', '十', '日'];
+if (isset($_GET['char']) && $_GET['char'] != null) {
+    $Char = $_GET['char'];
+} else {
+    $Char = $TestData[mt_rand(0, count($TestData) - 1)];
 }
-$OutputSize = min(512, empty($_GET['size'])?36:intval($_GET['size']));
+$OutputSize = min(512, empty($_GET['size']) ? 36 : intval($_GET['size']));
 //Demo start
-require(dirname(__FILE__) . "/src/MaterialDesign.Avatars.class.php");
+require(__DIR__ . "/vendor/autoload.php");
 
-$Avatar = new MDAvtars($Char, 512);
+$Avatar = new Md\MDAvatars($Char, 512);
 $Avatar->Output2Browser($OutputSize);
 $Avatar->Save('./avatars/Avatar256.png', 256);
 $Avatar->Save('./avatars/Avatar128.png', 128);
